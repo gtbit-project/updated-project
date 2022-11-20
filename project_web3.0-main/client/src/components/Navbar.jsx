9073eb1e9c9b354c8fc00889f618a4f640dab71b
sagar <sag28.01.2001@gmail.com>
2022-11-20 13:34:02 +0530

    FINAL uPDATED GIT

--- project_web3.0-main/client/src/components/Navbar.jsx
+++ project_web3.0-main/client/src/components/Navbar.jsx
@@ -2,8 +2,7 @@ import React from "react";
 import { HiMenuAlt4 } from "react-icons/hi";
 import { AiOutlineClose } from "react-icons/ai";
 
-import logo from "../../images/logo.png";
-
+// import logo from "../../images/logo.png";
 const NavBarItem = ({ title, classprops }) => (
   <li className={`mx-4 cursor-pointer ${classprops}`}>{title}</li>
 );
@@ -14,7 +13,8 @@ const Navbar = () => {
   return (
     <nav className="w-full flex md:justify-center justify-between items-center p-4">
       <div className="md:flex-[0.5] flex-initial justify-center items-center">
-        <img src={logo} alt="logo" className="w-32 cursor-pointer" />
+        <h1 style= {{color:"white",fontSize:"45px",fontFamily:"serif"}}>CryptSecure</h1>
+        {/* <img src={logo} alt="logo" className="w-32 cursor-pointer" /> */}
       </div>
       <ul className="text-white md:flex hidden list-none flex-row justify-between items-center flex-initial">
         {["Market", "Exchange", "Tutorials", "Wallets"].map((item, index) => (
--- project_web3.0-main/client/src/components/Welcome.jsx
+++ project_web3.0-main/client/src/components/Welcome.jsx
@@ -109,6 +109,8 @@ const Welcome = () => {
                   onClick={handleSubmit}
                   className="text-white w-full mt-2 border-[1px] p-2 border-[#3d4f7c] hover:bg-[#3d4f7c] rounded-full cursor-pointer"
                 >
+
+                  
                   Send now
                 </button>
               )}
